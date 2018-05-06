3def83da4845773bb3c078df0cf7ad5f248775e2
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-05-06 13:18:55 +0530

    Initialisation of variables

--- scr.cpp
+++ scr.cpp
@@ -44,4 +44,28 @@ game()			//_____________________________________________________________________
 
 }obj[4];// 4 objects as 4 players are defined
 //_____________________________________________________________________________________________________________________
+//________________________________________________Initialisation_Of_Game_Class_Variables_______________________________
+int game::chance=100;
+int game::testNumber=-1;
+char game::alpha[100]={
+'a','a','a','a','a','a','a','a','b','b',
+'c','d','d','d','d','d','e','e','e','e',
+'e','e','e','f','f','g','g','g','h','h',
+'i','i','i','i','i','j','k','k','k','l',
+'l','l','l','l','m','m','m','n','n','n',              // 100 letters  character set for game
+'n','n','n','o','o','o','o','o','p','p',
+'q','q','q','r','r','r','r','r','s','s',
+'s','s','s','s','s','s','t','t','t','t',
+'t','t','t','t','u','u','u','v','v','w',
+'w','x','x','y','y','z','z','z','i','a'};
+int game::scoreUser1=0;
+int game::scoreUser2=0;
+int game::scoreUser3=0;
+int game::scoreUser4=0;
+int userNumber=0; 
+//____________________________________________Global_Variable_and_Method_______________________________________________
+
+char names[4][20];		// new 2d variable array to store names of players
+void winner();			// function to calculate winner
+
 //_____________________________________________________________________________________________________________________
