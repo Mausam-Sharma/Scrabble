491f750a7f2f9e09c1885303385e2d2396ba8105
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-05-06 13:06:04 +0530

    game class member variables defined

--- scr.cpp
+++ scr.cpp
@@ -11,3 +11,25 @@
 #include<ctype.h>
 #include<time.h>
 //--------------------------------------------------------------------------------------------------------------------
+//_____________________________________________GAME__CLASS______MEMBERS_DEFINED_______________________________________
+ class game{  public:
+
+char letter1[7],play1[20];                //letter1 is the buffer for user characters, play1 is used to store user name
+
+int j[7],leng1,score1,flag;  int a,b;
+
+static char alpha[100];
+static int chance;
+static int testNumber;
+
+static int scoreUser1,scoreUser2,scoreUser3,scoreUser4;               //scores of users
+game()			//___________________________________________________________________________constructor_______________
+{  leng1=0,score1=0;
+
+
+
+}
+
+
+}obj[4];// 4 objects as 4 players are defined
+//____________
