de140560b0dd4d7db0b899e6f5aa08e01bb415c8
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2018-05-06 13:00:41 +0530

    new file

---
+++ scr.cpp
@@ -0,0 +1,13 @@
+//---------------------------------------------HEADER FILES NECESSARY-------------------------------------------------
+#include<stdio.h>
+#include<iostream.h>
+#include<fstream.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+#include<process.h>
+#include<string.h>                      //header files necessary
+#include<stdlib.h>
+#include<ctype.h>
+#include<time.h>
+//--------------------------------------------------------------------------------------------------------------------
